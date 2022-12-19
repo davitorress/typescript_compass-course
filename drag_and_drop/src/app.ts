e5e25d5bf46059d30db320250ce18d275bdc6f1f
davitorress <dev.davitorres@gmail.com>
2022-12-19 18:03:00 -0300

    drag and drop project

---
+++ drag_and_drop/src/app.ts
@@ -0,0 +1,9 @@
+/* Drag and Drop Project */
+
+// Using ES6 Modules
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
+
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
